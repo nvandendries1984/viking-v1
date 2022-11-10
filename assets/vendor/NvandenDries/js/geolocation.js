dcbe0ec5306b71460850950fe8adbcaa3468a129
codeskullz <niels@nvandendries.nl>
2022-11-10 17:18:36 +0100

    Geolocation Added

---
+++ assets/vendor/NvandenDries/js/geolocation.js
@@ -0,0 +1,15 @@
+window.onload = getLocation;
+
+var x = document.getElementById("geoloc");
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition)
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    x.setAttribute('value', position.coords.latitude + ", " + position.coords.longitude)
+}
